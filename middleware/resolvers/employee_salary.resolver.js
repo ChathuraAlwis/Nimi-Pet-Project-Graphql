0d3157ee33f4ba6ff47bbbef652d7535c6fed83a
Chathura Alwis <chathura7alwis@gmail.com>
2022-10-31 15:17:27 +0530

    Delete row functionality initial commit

--- middleware/resolvers/employee_salary.resolver.js
+++ middleware/resolvers/employee_salary.resolver.js
@@ -10,4 +10,20 @@ module.exports = {
       return salary_records
     },
   },
+
+  Mutation: {
+    delete_record: async(parent, args) => {
+      try {
+        const deleted_record = await prisma.employee_salary.delete({
+          where: {
+            id: parseInt(args.id)
+          }
+        })
+        return deleted_record
+      } catch (error) {
+        return null
+      }
+    }
+  }
+
 }
