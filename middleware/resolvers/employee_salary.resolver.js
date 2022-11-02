a2fb96f713956cf6db57d753775d7c5a1c7b4529
Chathura Alwis <chathura7alwis@gmail.com>
2022-11-02 11:09:51 +0530

    Resolver for sending download link

--- middleware/resolvers/employee_salary.resolver.js
+++ middleware/resolvers/employee_salary.resolver.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
-
+const { server_config } = require('../../src/config/server.config')
 
 
 module.exports = {
@@ -9,5 +9,20 @@ module.exports = {
       const salary_records = await prisma.employee_salary.findMany()
       return salary_records
     },
+    
+    download_record: async(parent, args) => {
+      const record_exists = await prisma.employee_salary.count({
+        where: {
+          id: parseInt(args.id)
+        }
+      })
+      if (!record_exists) throw new Error("Record does not exists!")
+
+      const download_link = {
+        url: `http://localhost:${server_config.port}/employee_salary/download/${args.id}`
+      }
+
+      return download_link
+    }
   },
 }
