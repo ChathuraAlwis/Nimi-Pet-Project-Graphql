d0358e00fdc152f60a13c21bee83a48a063a39a2
Chathura Alwis <chathura7alwis@gmail.com>
2022-11-02 11:11:44 +0530

    Integrated apollo-server with express

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-const { ApolloServer } = require("apollo-server");
+const express = require('express')
+const { ApolloServer } = require("apollo-server-express");
 const fs = require('fs');
 const path = require('path');
+const { server_config } = require('./config/server.config')
+const router = require('./routes/router')
 
 // resolvers
 const employee_salary_resolver = require('../middleware/resolvers/employee_salary.resolver')
 
+async function createApp(){
+  const server = new ApolloServer({
+    typeDefs: fs.readFileSync(
+      path.join(__dirname, '/../middleware/schema/employee_salary.schema.graphql'),
+      'utf8'
+    ),
+    resolvers: employee_salary_resolver,
+  })
+  
+  const app = express()
+  app.use('/employee_salary', router)
 
-const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, '/../middleware/schema/employee_salary.schema.graphql'),
-    'utf8'
-  ),
-  resolvers: employee_salary_resolver,
-})
+  
+  await server.start()
+  server.applyMiddleware({ app })
+  
+  app.listen({ port: server_config.port }, () => console.log(`Server is running on http://localhost:${server_config.port}${server.graphqlPath}`))
+}
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+createApp()
