03081f06d8a54bf02190246d7d4342637f5a1e27
Chathura Alwis <chathura7alwis@gmail.com>
2022-11-03 12:10:40 +0530

    Resolver for update query

--- middleware/resolvers/employee_salary.resolver.js
+++ middleware/resolvers/employee_salary.resolver.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
-
+const dynamic_update_data = require('../../src/utils/dynamic_update_data.util')
 
 
 module.exports = {
@@ -10,4 +10,18 @@ module.exports = {
       return salary_records
     },
   },
+
+  Mutation: {
+    update: async(parent, args) => {
+      const record_exists = await prisma.employee_salary.count({where: {id: parseInt(args.id)}})
+      if (!record_exists) throw new Error("Record doesn't exists!")
+      const updated_record = await prisma.employee_salary.update({
+        where:{
+          id: parseInt(args.id)
+        },
+        data: dynamic_update_data(args)
+      })
+      return updated_record
+    }
+  }
 }
