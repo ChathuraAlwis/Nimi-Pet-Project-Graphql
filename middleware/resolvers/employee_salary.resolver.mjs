9e17369d7a62e0dd3b6847b9906aa9272fc5f965
Chathura Alwis <chathura7alwis@gmail.com>
2022-11-03 20:45:15 +0530

    Moved download_record from Mutation to Query

--- middleware/resolvers/employee_salary.resolver.mjs
+++ middleware/resolvers/employee_salary.resolver.mjs
@@ -11,6 +11,20 @@ const emp_salary_resolver = {
       const salary_records = await prisma.employee_salary.findMany()
       return salary_records
     },
+    download_record: async(parent, args) => {
+      const record_exists = await prisma.employee_salary.count({
+        where: {
+          id: parseInt(args.id)
+        }
+      })
+      if (!record_exists) throw new Error("Record does not exists!")
+
+      const download_link = {
+        url: `http://localhost:${server_config.port}/employee_salary/download/${args.id}`
+      }
+
+      return download_link
+    }
   },
 
   Mutation: {
@@ -29,20 +43,6 @@ const emp_salary_resolver = {
         return null
       }
     },
-    download_record: async(parent, args) => {
-      const record_exists = await prisma.employee_salary.count({
-        where: {
-          id: parseInt(args.id)
-        }
-      })
-      if (!record_exists) throw new Error("Record does not exists!")
-
-      const download_link = {
-        url: `http://localhost:${server_config.port}/employee_salary/download/${args.id}`
-      }
-
-      return download_link
-    }
   }
 }
 
