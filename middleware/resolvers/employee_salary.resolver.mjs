c4a7f144c4b3a7c67c9a66c8b9110c9b9eb58dfd
Chathura Alwis <chathura7alwis@gmail.com>
2022-11-04 12:57:04 +0530

    Used custom error

--- middleware/resolvers/employee_salary.resolver.mjs
+++ middleware/resolvers/employee_salary.resolver.mjs
@@ -3,7 +3,7 @@ const require = createRequire(import.meta.url);
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
 const dynamic_update_data = require('../../src/utils/dynamic_update_data.util')
-
+import { GraphQLError } from 'graphql';
 
 const emp_salary_resolver = {
   Query: {
@@ -15,15 +15,27 @@ const emp_salary_resolver = {
 
   Mutation: {
     update: async(parent, args) => {
-      const record_exists = await prisma.employee_salary.count({where: {id: parseInt(args.id)}})
-      if (!record_exists) throw new Error("Record doesn't exists!")
-      const updated_record = await prisma.employee_salary.update({
-        where:{
-          id: parseInt(args.id)
-        },
-        data: dynamic_update_data(args)
-      })
-      return updated_record
+      try {
+        const updated_record = await prisma.employee_salary.update({
+          where:{
+            id: parseInt(args.id)
+          },
+          data: dynamic_update_data(args)
+        })
+        return updated_record
+      } catch (error) {
+        throw new GraphQLError(error.meta.cause, {
+          extensions:{
+            success: false,
+            error: {
+              statusCode: 500,
+              prismaErrorCode: error.code,
+              message: "INTERNAL_SERVER_ERROR",
+              stack: error.stack
+            },
+          }
+        });
+      }
     }
   }
 }
